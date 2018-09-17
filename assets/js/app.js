87c96cd428f2e512e5f4cace0eb3db26cc863a79
PMack2 <Prestonmack@gmail.com>
2018-09-17 13:12:04 -0400

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,13 +24,33 @@ var questions = [{
     option4: "Cleveland Indians",
     answer: 1
 
+},{
+
+    question: "What baseball great is known for such colorful sayings as 'It ain't over till it's over'?",
+    option1: "Yogi Berra",
+    option2: "Whitey Herzog",
+    option3: "Billy Martin",
+    option4: "Honus Wagner",
+    answer: 0
+
 
 }];
 
 
-var currentQuestion = 0
-var score=0;
+var messages = {
+	correct: "YES! You are right!",
+	incorrect: "STRIKE! You missed it.",
+	endTime: "You're Out...    of time!",
+	finished: "Alright! Let's look at the scorecard..."
+}
+
+var currentQuestion = 0;
+var correctAnswer =0;
+var incorrectAnswer =0;
+var unanswered = 0;
 var totQuestions = questions.length;
+var seconds = 0;
+var time = 0;
 
 
 var container = document.getElementById("quizContainer");
@@ -44,6 +64,25 @@ var nextButton = document.getElementById(nextButton);
 var resultContainer = document.getElementById(result);
 
 
+$('#startBtn').on('click', function(){
+    $(this).hide();
+    newGame();
+});
+
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+}
+
+
+
+
 function loadQuestion(questionIndex){
     var q = questions[questionIndex];
     questionEl.textContent = (questionIndex + 1) + " . " + q.question;
@@ -51,29 +90,44 @@ function loadQuestion(questionIndex){
     opt2.textContent = q.option2;
     opt3.textContent = q.option3;
     opt4.textContent = q.option4;
+    $("#currentQuestion").html("Question #"+(currentQuestion+1)+" / "+questions.length);
+    
 }
 
-function loadNextQuestion(){
+function checkAnswer(){
     var selectedOption = document.querySelector("input[type=radio]:checked");
     if(!selectedOption){
         alert("Please select answer");
         return;
     }
     var answer = selectedOption.value;
-    if(triviaQuestions[currentQuestion].answer == answer){
+    if(questions[currentQuestion].answer == answer){
         correctAnswer++;
+        console.log(correctAnswer);
+        currentQuestion++;
+        $('#message').html(messages.correct);
+        console.log(messages.correct);
     }
     selectedOption.checked = false;
     currentQuestion++;
-    if(currentQuestion == totQuestions - 1){
-        nextButton.textContent = "finish";
-    }
+    incorrectAnswer--;
+    console.log(incorrectAnswer);
+    $('#message').html(messages.incorrect);
+    console.log(messages.incorrect);
+
 
     if(currentQuestion == totQuestions){
         container.style.display="none";
-        resultContainer.style.display = "";
-        resultContainer.textContent= "Your score" + score;
-    
+        $('#finalMessage').show();
+        $('#finalMessage').html(messages.finished);
+        $('#correctAnswers').show();
+        $('#correctAnswers').html("Correct = " + correctAnswer);
+        $('#incorrectAnswers').show();
+        $('#incorrectAnswers').html("Incorrect = " + incorrectAnswer);
+        $('#unanswered').show();
+        $('#unanswered').html("Unanswered = " + unanswered);
+        
+        return;
     }
     loadQuestion(currentQuestion);
 }
